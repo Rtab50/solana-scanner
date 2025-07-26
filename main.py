8293937694f72ab725071eb134dfcb8ec245b7b4
Rtab50 <tabeshgold@gmail.com>
2025-07-26 11:12:31 +0330

    main.py

--- main.py
+++ main.py
@@ -68,6 +68,6 @@ def index():
 
     return render_template("index.html", tokens=results)
        if __name__ == "__main__":
-          import os
-            port = int(os.environ.get("PORT", 5000))
-            app.run(host="0.0.0.0", port=port)
+           import os
+           port = int(os.environ.get("PORT", 5000))
+           app.run(host="0.0.0.0", port=port)
